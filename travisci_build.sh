2a205b368839260faf8e26b8695d51277e6ea0c0
Oleksii Khilkevych <o.khilkevich@gastrofix.com>
2015-05-15 20:21:28 +0200

    test on push

--- travisci_build.sh
+++ travisci_build.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "==== This was not a pull request, nothing executed! ===="
-else
-  ./gradlew pullRequestValidation
-fi
+./gradlew test
